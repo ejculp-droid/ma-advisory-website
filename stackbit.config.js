55b3b7cb1502b4a6c985674d3b0a0c368412eb7f
ejculp-droid <ejculp@gmail.com>
2026-01-28 21:39:52 -0500

    Refactor stackbit.config.js to use GitContentSource

--- stackbit.config.js
+++ stackbit.config.js
@@ -1,15 +1,42 @@
-export default {
+import { defineStackbitConfig } from '@stackbit/types';
+import { GitContentSource } from '@stackbit/cms-git';
+
+export default defineStackbitConfig({
   stackbitVersion: '~0.6.0',
   ssgName: 'custom',
   nodeVersion: '18',
-  contentSources: [],
-  models: {
-    page: {
-      type: 'page',
-      urlPath: '/{slug}',
-      filePath: 'pages/{slug}.html'
-    }
-  },
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ['content'],
+      models: [
+        {
+          name: 'Page',
+          type: 'page',
+          urlPath: '/{slug}',
+          filePath: 'content/pages/{slug}.json',
+          fields: [
+            { name: 'title', type: 'string', required: true },
+            { name: 'heroTitle', type: 'string' },
+            { name: 'heroSubtitle', type: 'text' },
+            { name: 'heroButtonText', type: 'string' },
+            { name: 'sections', type: 'list', items: { type: 'object' } }
+          ]
+        },
+        {
+          name: 'SiteConfig',
+          type: 'data',
+          filePath: 'content/data/config.json',
+          fields: [
+            { name: 'siteName', type: 'string' },
+            { name: 'phone', type: 'string' },
+            { name: 'email', type: 'string' },
+            { name: 'address', type: 'text' }
+          ]
+        }
+      ]
+    })
+  ],
   pagesDir: 'pages',
   dataDir: 'content'
-};
+});
